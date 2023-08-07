7ed821a02a4f8f42907e756f0189053112f1b469
AUCKLAND\Priyank.Kothari <priyank.kothari@vista.co>
2023-08-07 15:30:25 +1200

    - Renamed WeatherApp.Core to WeatherApp.Domain - Added CurrentWeather and CurrentWeatherQuery domain models.

---
+++ WeatherApp.Domain/DomainModels/CurrentWeather.cs
@@ -0,0 +1,28 @@
+﻿namespace WeatherApp.Core.Entities
+{
+    /// <summary>
+    /// Represents a weather entity.
+    /// </summary>
+    public sealed class CurrentWeather
+    {
+        /// <summary>
+        /// Gets or sets a City Name.
+        /// </summary>
+        public string CityName { get; init; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets weather condition description.
+        /// </summary>
+        public string Description { get; init; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets minimum temperature.
+        /// </summary>
+        public double MinimumTemperature { get;}
+
+        /// <summary>
+        /// Gets or sets maximum temperature.
+        /// </summary>
+        public double MaximumTemperature { get;}
+    }
+}
---
+++ WeatherApp.Domain/DomainModels/CurrentWeatherQuery.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+
+namespace WeatherApp.Domain.DomainModels
+{
+    /// <summary>
+    /// Query object to get current weather.
+    /// </summary>
+    public sealed class CurrentWeatherQuery
+    {
+        /// <summary>
+        /// Gets or sets a city name.
+        /// </summary>
+        public string CityName { get; set; }
+
+        public string StateCode { get; set; }
+
+        public string CountryCode { get; set; }
+
+        public CurrentWeatherQuery(string cityName, string stateCode, string countryCode)
+        {
+            CityName = cityName;
+            StateCode = stateCode;
+            CountryCode = countryCode;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder currentWeatherQuery = new StringBuilder();
+
+            currentWeatherQuery.Append(CityName);
+
+            // append StateCode only if it is not null or white space.
+            if (!string.IsNullOrWhiteSpace(StateCode))
+            {
+                currentWeatherQuery.Append($", {StateCode}");
+            }
+
+            // append CountryCode only if it is not null or white space.
+            if (!string.IsNullOrWhiteSpace(CountryCode))
+            {
+                currentWeatherQuery.Append($", {CountryCode}");
+            }
+
+            return currentWeatherQuery.ToString();
+        }
+    }
+}
